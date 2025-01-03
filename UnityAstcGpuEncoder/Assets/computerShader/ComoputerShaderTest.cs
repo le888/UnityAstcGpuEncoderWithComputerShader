c8893f331eee3a31596063b52465f746b6d7cff3
liuleyou <707649841@qq.com>
2025-01-03 18:51:21 +0800

    0.1

--- UnityAstcGpuEncoder/Assets/computerShader/ComoputerShaderTest.cs
+++ UnityAstcGpuEncoder/Assets/computerShader/ComoputerShaderTest.cs
@@ -1,15 +1,27 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 
 public class ComoputerShaderTest : MonoBehaviour
 {
     public ComputeShader computeShader;
     public Material material;
     // Start is called before the first frame update
-    void Start()
+    int imageSize = 2048;
+    RenderTexture tex;
+    private void OnEnable()
     {
-        
+        tex = new RenderTexture(imageSize, imageSize, 0);
+        tex.enableRandomWrite = true;
+        tex.graphicsFormat = GraphicsFormat.R8G8B8A8_UNorm;
+        tex.Create();
+    }
+
+    private void OnDisable()
+    {
+        tex?.Release();
     }
 
     // Update is called once per frame
@@ -21,11 +33,10 @@ public class ComoputerShaderTest : MonoBehaviour
         }
         
         int kernelHandle = computeShader.FindKernel("CSMain");
-        RenderTexture tex = new RenderTexture(256, 256, 24);
-        tex.enableRandomWrite = true;
-        tex.Create();
+        
         computeShader.SetTexture(kernelHandle, "Result", tex);
-        computeShader.Dispatch(kernelHandle, 256 / 8, 256 / 8, 1);
+        computeShader.SetFloat("imageSize", imageSize);
+        computeShader.Dispatch(kernelHandle, imageSize / 8, imageSize / 8, 1);
 
         if (material != null)
         {
